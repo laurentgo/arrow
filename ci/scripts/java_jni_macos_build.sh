947e1cf7cafc8f104e21f2cf642fc929dfc00e56
Laurent Goujon <laurent@apache.org>
2023-06-28 08:54:12 -0700

    Exclude some macos unstable tests

--- ci/scripts/java_jni_macos_build.sh
+++ ci/scripts/java_jni_macos_build.sh
@@ -98,7 +98,7 @@ if [ "${ARROW_BUILD_TESTS}" == "ON" ]; then
   # MinIO is required
   exclude_tests="arrow-s3fs-test"
   # unstable
-  exclude_tests="${exclude_tests}|arrow-acero-hash-join-node-test"
+  exclude_tests="${exclude_tests}|arrow-acero-hash-join-node-test|arrow-substrait-substrait-test|arrow-acero-asof-join-node-test"
   ctest \
     --exclude-regex "${exclude_tests}" \
     --label-regex unittest \
